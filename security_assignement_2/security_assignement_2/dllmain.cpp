b8f3a9177481ea2690efc149916e189fff0e5932
AndrewF001 <andrewfoot@hotmail.com>
2023-11-16 11:50:33 +0000

    added console to DLL

--- security_assignement_2/security_assignement_2/dllmain.cpp
+++ security_assignement_2/security_assignement_2/dllmain.cpp
@@ -5,12 +5,20 @@
 #include <Windows.h>
 
 void fake_main() {
+
+    // Only works if we know base address for the server
     const HMODULE server_dll_base_addr = GetModuleHandle(L"server.dll");
-    
     if (server_dll_base_addr == nullptr) {
         throw "failed to find a loaded server.dll";
     }
     
+    // open console
+    AllocConsole();
+    FILE* pFile;
+    freopen_s(&pFile, "CONOUT$", "w", stdout);
+    freopen_s(&pFile, "CONIN$", "r", stdin);
+    std::cout << "DLL attached\n";
+
     // Our game-mode here
 }
 
